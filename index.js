5efbe362d564b338010a35482a522a3623cf7206
Harpreet <hkaur108@hotmail.com>
2024-07-03 13:44:28 -0400

    second commit of shopping cart

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ class UI{
             <i class="fas fa-chevron-up" data-id=${item.id}></i>
     
         <p class="item-amount">${item.amount}</p>
-    
+
             <i class="fas fa-chevron-down" data-id=${item.id}></i>
 
         </div>`
